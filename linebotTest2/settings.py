cf00c296ec72ab4228252c4fcd9b4f323b8386ba
oppq1234 <oppq1234@gmail.com>
2020-08-27 15:30:46 +0800

    change db

--- linebotTest2/settings.py
+++ linebotTest2/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'linebotTest2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'userDB',
+        'USER': 'user',
+        'PASSWORD': 'user123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
